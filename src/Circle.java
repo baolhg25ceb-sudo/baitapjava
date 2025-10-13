fc636735e9953a4b6485b5bbe142e79019022a9d
Le Ha Gia Bao <baolhg.25ceb@vku.udn.vn>
2025-10-13 13:47:19 +0700

    init project

---
+++ src/Circle.java
@@ -0,0 +1,47 @@
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double r) {
+        this.radius = r;
+        this.color = "red";
+    }
+
+    public Circle(double r, String color) {
+        this.radius = r;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setRadius(double r) {
+        this.radius = r;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public String toString() {
+        return "Circle [radius=" + this.radius + ", color=" + this.color + "]";
+    }
+}
